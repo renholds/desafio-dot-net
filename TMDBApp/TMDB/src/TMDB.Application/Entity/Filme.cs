e931426f203605d70ccbf1a0d6ddf3ebff4188f8
renholds <58077899+renholds@users.noreply.github.com>
2019-12-09 19:04:50 -0300

    Criação do Projeto de Catálogo de Filmes WebApp
    Criação do Projeto de Catálogo de Filmes WebApp
    
    TMDBWebApp

---
+++ TMDBApp/TMDB/src/TMDB.Application/Entity/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMDB.Application.Entity
+{
+    public class Filme
+    {
+        public Filme()
+        {
+
+        }
+
+        public int FilmeId { get; set; }
+        public String Nome { get; set; }
+    }
+}
